69a35b7aeed018061d25c5db52ce36febe987069
Mikhail Karpenko <70774954+mrmeihem@users.noreply.github.com>
2022-02-23 14:48:22 +0200

    Create Menu.js

---
+++ src/utils/Menu.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import { IconButton, Drawer } from '@mui/material';
+import {
+  Menu as MenuIcon,
+  ArrowBack as ArrowBackIcon,
+} from '@mui/icons-material';
+
+// Компонент принимает в себя другой компонент в качестве пропса и выводит его в меню.
+// Оберните свой компонент BOX из MUI с нужной шириной. 
+
+export default function TemporaryDrawer({ menuContent }) {
+  const MenuContent = menuContent;
+
+  const [state, setState] = useState(false);
+
+  const toggleDrawer = () => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    )
+      return;
+    setState(!state);
+  };
+
+  return (
+    <>
+      <IconButton onClick={toggleDrawer()}>
+        {state ? <ArrowBackIcon /> : <MenuIcon />}
+      </IconButton>
+      <Drawer open={state} onClose={toggleDrawer()}>
+        <MenuContent />
+      </Drawer>
+    </>
+  );
+}
