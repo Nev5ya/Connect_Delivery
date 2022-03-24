3d12d0da13882f47e5bd62cd4f47c3f12372905a
Alex <75433612+Alex52-62@users.noreply.github.com>
2022-03-24 17:06:26 +0300

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,34 +1,19 @@
 import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { login, signUp } from "../../services/firebase";
+import { useSelector} from "react-redux";
+import { login} from "../../services/firebase";
 import { ref, set, onValue } from "firebase/database";
-import Checkbox from "@mui/material/Checkbox";
-
-import { toggleLogReg } from "../../store/home/actions";
 import { db } from "../../services/firebase";
 // import "./style.css";
-import {Redirect} from "react-router-dom";
 
 const onLogin = async (email, pass) => {
   try {
     await login(email, pass);
-    // setAuthed(true);
   } catch (e) {
     alert("We haven't such login, please re-enter");
     console.log(e);
   }
 };
 
-const onSignUp = async (email, pass) => {
-  try {
-    await signUp(email, pass);
-    // setAuthed(true);
-    return <Redirect to="/Profile" />;
-  } catch (e) {
-    console.log(e);
-    alert("You enter wrong login or password, please check conditions ");
-  }
-};
 
 export const Home = () => {
   const [login, setLogin] = useState("");
@@ -43,11 +28,6 @@ export const Home = () => {
   };
 
   const showReg = useSelector((state) => state.home.showReg);
-  const dispatch = useDispatch();
-
-  const handleClick = () => {
-    dispatch(toggleLogReg);
-  };
 
   useEffect(() => {
     const profileDbRef = ref(db, "profile/login");
@@ -62,13 +42,8 @@ export const Home = () => {
     e.preventDefault();
     setLogin("");
     setPass("");
+    onLogin(login, pass);
 
-    if (showReg) {
-      onLogin(login, pass);
-    } else {
-      onSignUp(login, pass);
-    }
- 
     set(ref(db, "profile/login"), {
       login: login,
     });
@@ -76,18 +51,12 @@ export const Home = () => {
 
   return (
       <div className="checkIn">
-        <div><h3>{showReg ? "Login" : "SignUp"}</h3></div>
+
         <div>
           <form className="checkInForm" onSubmit={handleSubmit}>
             <input type="text" value={login} onChange={handleLoginChange} />
             <input type="password" value={pass} onChange={handlePassChange} />
             <input type="submit" />
-            <Checkbox
-              onChange={handleClick}
-              inputProps={{ "aria-label": "primary checkbox" }}
-              defaultChecked
-            />
-            {showReg && <div className="SN"></div>}
             </form>
         </div>
         <div><img src="../images/mainP.jpg" alt="logo"></img></div>
