c0025acaaea28fa4b82107a4d55f6eee4ed8f63a
Mikhail Karpenko <70774954+mrmeihem@users.noreply.github.com>
2022-04-05 21:17:33 +0300

    UPD2. Admin page navigation corrections. Minor adjustments. Code cleanup.

--- src/components/AdminTable/index.js
+++ src/components/AdminTable/index.js
@@ -1,45 +1,48 @@
-import * as React from 'react';
+import {Route, Routes} from "react-router-dom";
 import {useEffect} from 'react';
-import MyMap from "../Map/map.js";
+
 import {useDispatch, useSelector} from "react-redux";
+
 import {getOrders} from "../../store/orders/actions";
 import {selectCouriers} from "../../store/couriers/selector";
 import {selectOrders} from "../../store/orders/selector";
 import {getCouriers} from "../../store/couriers/actions";
-import {Chat} from "../Chat/Chat";
-import {CouriersOperation} from "../AdminTable/CouriersOperation/CouriersOperation";
-import {CourierRegistration} from "../AdminTable/CourierRegistration/CourierRegistration";
-import {Route, Routes} from "react-router-dom";
+
 import {AdminHeader} from "./AdminHeader/AdminHeader";
+
 import {AdminMain} from "./AdminMain/AdminMain";
+import {Chat} from "../Chat/Chat";
+import MyMap from "../Map/map";
+import {CouriersOperation} from "./CouriersOperation/CouriersOperation";
+import {CourierRegistration} from "./CourierRegistration/CourierRegistration";
 
 export const AdminTable = () => {
 
-  const dispatch = useDispatch();
-  useEffect((event) => {
-    console.log('useEffect')
-    dispatch(getOrders());
-    dispatch(getCouriers());
-  }, []);
+    const dispatch = useDispatch();
 
-  const orders = useSelector(selectOrders)
-   console.log('state orders', orders)
-  const couriers = useSelector(selectCouriers)
-   console.log('state couriers', couriers)
+    useEffect(() => {
+        dispatch(getOrders());
+        dispatch(getCouriers());
+    }, []);
 
-  return (
-      <>
-        <AdminHeader/>
+    const orders = useSelector(selectOrders)
+    const couriers = useSelector(selectCouriers)
 
-        <Routes>
-            <Route index element={<AdminMain />} />
-            <Route path="Chat" element={<Chat  mode="Admin" />} />
-            <Route path="MyMap" element={<MyMap  name={"Местонахождение курьеров и заказов"} couriers={couriers} orders={orders} />} />
-            <Route path="CouriersOperation" element={<CouriersOperation />} />
-            <Route path="CourierRegistration" element={<CourierRegistration />} />
-        </Routes>
+    return (
+        <>
+            <AdminHeader/>
+            <Routes>
+                <Route index element={<AdminMain/>}/>
+                <Route path="Chat" element={<Chat mode="Admin"/>}/>
+                <Route path="MyMap" element={<MyMap name={"Местонахождение курьеров и заказов"}
+                                                    couriers={couriers}
+                                                    orders={orders}
+                                             />}
+                />
+                <Route path="CouriersOperation" element={<CouriersOperation/>}/>
+                <Route path="CourierRegistration" element={<CourierRegistration/>}/>
+            </Routes>
 
-      </>
-  );
+        </>
+    );
 };
-
